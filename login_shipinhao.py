594366e0f9d96879a6370191f087bab165898b40
Lachlan Chen <lachlan.mia.chan@gmail.com>
2024-02-23 09:24:25 +0800

    fix NoSuchElementException error

--- login_shipinhao.py
+++ login_shipinhao.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 
 # from sendgrid import SendGridAPIClient
