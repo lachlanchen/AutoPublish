3f14e0b8565147ebb21bee1d674d83024e553f69
Lachlan Chen <lachlan.mia.chan@gmail.com>
2024-06-09 12:47:48 +0800

    fix xhs location

--- pub_xhs.py
+++ pub_xhs.py
@@ -255,8 +255,8 @@ class XiaoHongShuPublisher:
                             print(f"Retrying to select location... Attempts left: {retry_count}")
                             select_location(driver, location_names[1:], retry_count - 1)  # Remove the first name and retry with the next one
                         else:
+                            print(f"Failed to select location after multiple attempts. Try another method. ")
                             select_location_b(driver, ['香港大学', 'The University of Hong Kong'])
-                            print(f"Failed to select location after multiple attempts.")
 
 
                 
