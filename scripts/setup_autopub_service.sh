28bf5940735b3a7e9dc903be0e391897bed44b6e
lazyingart <lachlan@lazying.art>
2026-03-25 21:55:11 +0800

    make autopub service installer mount-aware

--- scripts/setup_autopub_service.sh
+++ scripts/setup_autopub_service.sh
@@ -8,6 +8,12 @@ REPO_DIR="${AUTOPUB_REPO:-$REPO_DIR_DEFAULT}"
 ENV_FILE="/etc/default/autopub"
 SERVICE_PATH="/etc/systemd/system/autopub.service"
 START_SCRIPT="$REPO_DIR/scripts/start_autopub_tmux.sh"
+HOME_PATH="/home/${USER_NAME}"
+DISKMECH_PATH="${HOME_PATH}/DiskMech"
+AUTOPUB_DATA_PATH="${HOME_PATH}/AutoPublishDATA"
+HOME_MOUNT_UNIT="home-${USER_NAME}.mount"
+DISKMECH_MOUNT_UNIT="home-${USER_NAME}-DiskMech.mount"
+AUTOPUB_DATA_MOUNT_UNIT="home-${USER_NAME}-AutoPublishDATA.mount"
 
 if [[ "$EUID" -ne 0 ]]; then
   echo "Please run as root: sudo -E $0"
@@ -21,7 +27,9 @@ fi
 cat > "$SERVICE_PATH" <<SERVICE
 [Unit]
 Description=AutoPublish tmux service
-After=network.target
+Wants=network-online.target
+After=network-online.target ${HOME_MOUNT_UNIT} ${DISKMECH_MOUNT_UNIT} ${AUTOPUB_DATA_MOUNT_UNIT}
+RequiresMountsFor=${HOME_PATH} ${DISKMECH_PATH} ${AUTOPUB_DATA_PATH}
 
 [Service]
 Type=oneshot
